e2dbe4b8c047765335b963e9f4e77d6593d66bee
Muhammet Can Zengin <canzengin@cans-macbook-pro.local>
2018-10-27 22:53:40 +0300

    Initial Commit

---
+++ Question1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Question1
+//
+//  Created by Can Zengin on 10/27/18.
+//  Copyright © 2018 Can Zengin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
